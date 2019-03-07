869454cf0fbcdfb3891707db44fa7d5dfc6f949c
GiselleSerate <giselle.serate@gmail.com>
2019-03-07 15:09:49 -0800

    Deploy merged changes

--- controller.js
+++ controller.js
@@ -7,19 +7,31 @@ let video;
 let poseNet;
 let poses = [];
 
+let pg;
+
 let noseX;
 let noseY;
 
 let pNoseX;
 let pNoseY;
 
+let magnitude;
+
 let jumpCooldown;
 let threshold;
+let mode;
 
+// Function that p5 calls initially to set up graphics
 function setup() {
-  createCanvas(640, 480);
+  var canvas = createCanvas(640, 480);
+  canvas.parent('videoContainer');
   video = createCapture(VIDEO);
   video.size(width, height);
+  video.parent('videoContainer')
+
+  pixelDensity(1);
+  pg = createGraphics(width, height);
+  pg.parent('videoContainer');
 
   // Create a new poseNet method with a single detection
   poseNet = ml5.poseNet(video, function() {});
@@ -28,15 +40,27 @@ function setup() {
     poses = results;
   });
 
-  video.hide();
+  // Show graphics
+  pg.show();
+
   jumpCooldown = 0;
-  threshold = 150; // TODO: tune this
+
+  // Set defaults
+  mode = "active";
+  delay = 7;
+  threshold = 150;
+  sensitivity = 40;
+}
+
+// Function that p5 calls repeatedly to render graphics
+function draw() {
+  findNose();
+  updateThreshold();
 }
 
 function findNose() {
   jumpCooldown = max(0, jumpCooldown - 1);
-  // var yDiff = 0; // By default don't register movement
-  var active = 0; // By default don't register movement
+  var trigger = 0; // By default don't register movement
   if(poses.length > 0) {
     // Only detect nose keypoint of first pose
     let nose = poses[0].pose.keypoints[0];
@@ -45,26 +69,46 @@ function findNose() {
       noseX = nose.position.x;
       noseY = nose.position.y;
 
-      // // Difference
-      // yDiff = pNoseY - noseY; 
-
       // Active zone
-      active = pNoseY > threshold && noseY < threshold;
+      if(mode == "active") {
+        trigger = pNoseY > threshold && noseY < threshold;
+      }
+      // Difference
+      else {
+        console.log(pNoseY);
+        console.log(noseY);
+        console.log(sensitivity);
+        trigger = (pNoseY - noseY) > sensitivity; 
+        console.log(trigger);
+        magnitude = pNoseY - noseY;
+      }
 
       pNoseX = noseX;
       pNoseY = noseY;
     }
   }
 
-  // if (jumpCooldown == 0 && yDiff > 20) {
-  if (jumpCooldown == 0 && active) {
-    jumpCooldown = 7; // Reset jump cooldown
+  if (jumpCooldown == 0 && trigger) {
+    jumpCooldown = delay; // Reset jump cooldown
     console.log("JUMP");
     gameInstance.SendMessage("Player", "JumpDiscrete");
+    // TODO: Make sure this hook-in works properly; use Magnitude (but ignore depending on game settings) in velocity mode
+    // if (mode == "active") {
+    //   gameInstance.SendMessage("Player", "JumpDiscrete");
+    // }
+    // else {
+    //   gameInstance.SendMessage("Player", "JumpWithMagnitude", magnitude);
+    // }
   }
 }
 
-function registerNoseFinder() {
-  // Locate nose at max frequency possible
-  setInterval(function() { findNose() }, 0);
+// Visually update the threshold value
+function updateThreshold() {
+  pg.clear();
+  // Only render line in active mode. 
+  if(mode == "active") { 
+    pg.stroke(230, 80, 0);
+    pg.strokeWeight(1);
+    pg.line(0, threshold, width, threshold);
+  }
 }
