2cbfe416ca7cd2bbfe52219e87374a18d0c00d93
GiselleSerate <giselle.serate@gmail.com>
2019-03-20 12:39:37 -0700

    Respace file

--- turtleChase/TurtleChase/Assets/WebGLTemplates/PoseNet/controller.js
+++ turtleChase/TurtleChase/Assets/WebGLTemplates/PoseNet/controller.js
@@ -32,129 +32,129 @@ let constantMag = true;
 
 // Function that p5 calls initially to set up graphics
 function setup() {
-  video = createCapture(VIDEO);
-  video.size(vidWidth, vidHeight);
-  video.parent('videoContainer')
+    video = createCapture(VIDEO);
+    video.size(vidWidth, vidHeight);
+    video.parent('videoContainer')
 
-  pixelDensity(1);
-  pg = createGraphics(vidWidth, vidHeight);
-  pg.parent('videoContainer');
+    pixelDensity(1);
+    pg = createGraphics(vidWidth, vidHeight);
+    pg.parent('videoContainer');
 
-  // Options for PoseNet
-  let options = { 
-    // imageScaleFactor: 0.3,
-    // outputStride: 16,
-    flipHorizontal: false,
-    // flipHorizontal: false,
-    minConfidence: 0.2,
-    maxPoseDetections: 1,
-    // scoreThreshold: 0.5,
-    scoreThreshold: 2,
-    nmsRadius: 20,
-    detectionType: 'single',
-    // multiplier: 0.75,
-  }
-  // Create a new poseNet method with a single detection
-  poseNet = ml5.poseNet(video, options, function() {});
+    // Options for PoseNet
+    let options = { 
+        // imageScaleFactor: 0.3,
+        // outputStride: 16,
+        flipHorizontal: false,
+        // flipHorizontal: false,
+        minConfidence: 0.2,
+        maxPoseDetections: 1,
+        // scoreThreshold: 0.5,
+        scoreThreshold: 2,
+        nmsRadius: 20,
+        detectionType: 'single',
+        // multiplier: 0.75,
+    }
+    // Create a new poseNet method with a single detection
+    poseNet = ml5.poseNet(video, options, function() {});
 
-  poseNet.on('pose', function(results) {
-    poses = results;
-    // console.log(poses);
-    // ticks++;
-    // console.log(ticks/(Date.now()-start));
-  });
+    poseNet.on('pose', function(results) {
+        poses = results;
+        // console.log(poses);
+        // ticks++;
+        // console.log(ticks/(Date.now()-start));
+    });
 
-  // Hide the video so we can render it flipped in the draw loop. 
-  video.hide();
+    // Hide the video so we can render it flipped in the draw loop. 
+    video.hide();
 
-  // Show graphics
-  pg.show();
-  // Flip graphics so you get proper mirroring of video and nose dot
-  pg.translate(vidWidth,0);
-  pg.scale(-1.0, 1.0);
+    // Show graphics
+    pg.show();
+    // Flip graphics so you get proper mirroring of video and nose dot
+    pg.translate(vidWidth,0);
+    pg.scale(-1.0, 1.0);
 
-  jumpCooldown = 0;
+    jumpCooldown = 0;
 
-  // Benchmarking code
-  ticks = 0;
-  start = Date.now();
+    // Benchmarking code
+    ticks = 0;
+    start = Date.now();
 }
 
 // Function that p5 calls repeatedly to render graphics
 function draw() {
-  pg.clear();
+    pg.clear();
 
-  // Render video
-  pg.image(video, 0, 0);
+    // Render video
+    pg.image(video, 0, 0);
 
-  findNose();
-  updateThreshold();
+    findNose();
+    updateThreshold();
 }
 
 function findNose() {
-  jumpCooldown = max(0, jumpCooldown - 1);
-  let trigger = 0; // By default don't register movement
-  if(poses.length > 0) {
-    // Only detect nose keypoint of first pose
-    let nose = poses[0].pose.keypoints[0];
-    noseX = nose.position.x;
-    noseY = nose.position.y;
+    jumpCooldown = max(0, jumpCooldown - 1);
+    let trigger = 0; // By default don't register movement
+    if(poses.length > 0) {
+        // Only detect nose keypoint of first pose
+        let nose = poses[0].pose.keypoints[0];
+        noseX = nose.position.x;
+        noseY = nose.position.y;
 
-    switch(mode) {
-      case "active":
-        trigger = pNoseY > threshold != noseY > threshold;
-        on = noseY < threshold;
-        break;
-      case "velocity":
-        rawMagnitude = (constantMag ? 1 : pNoseY - noseY);
-        trigger = (pNoseY - noseY) > sensitivity[0]; 
-        break;
-      default:
+        switch(mode) {
+            case "active":
+                trigger = pNoseY > threshold != noseY > threshold;
+                on = noseY < threshold;
+                break;
+            case "velocity":
+                rawMagnitude = (constantMag ? 1 : pNoseY - noseY);
+                trigger = (pNoseY - noseY) > sensitivity[0]; 
+                break;
+            default:
+        }
+
+        pNoseX = noseX;
+        pNoseY = noseY;
+
+        // Render nose dot
+        pg.stroke(0, 225, 0);
+        pg.strokeWeight(5);
+        pg.ellipse(noseX, noseY, 1, 1);
+
+        // Benchmarking code
+        // ticks++;
+        // console.log(ticks/(Date.now()-start));
     }
 
-    pNoseX = noseX;
-    pNoseY = noseY;
-
-    // Render nose dot
-    pg.stroke(0, 225, 0);
-    pg.strokeWeight(5);
-    pg.ellipse(noseX, noseY, 1, 1);
-
-    // Benchmarking code
-    // ticks++;
-    // console.log(ticks/(Date.now()-start));
-  }
-
-  if (jumpCooldown == 0 && trigger) {
-    jumpCooldown = delay; // Reset jump cooldown
-    switch(mode) {
-      case "active":
-        if(on) {
-          console.log("active: JumpEnter")
-          // gameInstance.SendMessage("Player", "JumpEnter", 1);
+    if (jumpCooldown == 0 && trigger) {
+        jumpCooldown = delay; // Reset jump cooldown
+        switch(mode) {
+            case "active":
+                if(on) {
+                    console.log("active: JumpEnter")
+                    // gameInstance.SendMessage("Player", "JumpEnter", 1);
+                }
+                else {
+                    console.log("active: JumpExit")
+                    // gameInstance.SendMessage("Player", "JumpExit");
+                }
+                break;
+            case "velocity":
+                // Scale magnitude to sensitivity range
+                console.log(sensitivity);
+                scaledMagnitude = rawMagnitude / (sensitivity[1] - sensitivity[0])
+                console.log("velocity: JumpEnter ("+scaledMagnitude+")");
+                break;
+            default:
         }
-        else {
-          console.log("active: JumpExit")
-          // gameInstance.SendMessage("Player", "JumpExit");
-        }
-        break;
-      case "velocity":
-        // Scale magnitude to sensitivity range
-        console.log(sensitivity);
-        scaledMagnitude = rawMagnitude / (sensitivity[1] - sensitivity[0])
-        console.log("velocity: JumpEnter ("+scaledMagnitude+")");
-        break;
-      default:
     }
-  }
 }
 
 // Visually update the threshold value
 function updateThreshold() {
-  // Only render line in active mode. 
-  if(mode == "active") { 
-    pg.stroke(230, 80, 0);
-    pg.strokeWeight(1);
-    pg.line(0, threshold, vidWidth, threshold);
-  }
+    // Only render line in active mode. 
+    if(mode == "active") { 
+        pg.stroke(230, 80, 0);
+        pg.strokeWeight(1);
+        pg.line(0, threshold, vidWidth, threshold);
+    }
 }
