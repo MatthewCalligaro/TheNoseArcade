d39edff39bc45aef596c947725dd6aea74a1b88d
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-04-09 21:53:50 -0700

    Implemented face control for sliders

--- turtleChase/TurtleChase/Assets/Scripts/UI/MenuButton.cs
+++ turtleChase/TurtleChase/Assets/Scripts/UI/MenuButton.cs
@@ -1,28 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// A menu button which can be selected by the face controller
+/// </summary>
 public class MenuButton : MonoBehaviour, IMenuItem
 {
-    private static readonly Color pressedColor = new Color(0.75f, 1, 1);
-    private static readonly Color releasedColor = Color.white;
+    /// <summary>
+    /// Color that the button takes on to show that it is selected
+    /// </summary>
+    private static readonly Color selectedColor = new Color(0.75f, 1, 1);
 
+    /// <summary>
+    /// Unselected buttor color
+    /// </summary>
+    private static readonly Color defaultColor = Color.white;
+
+    /// <summary>
+    /// Handles when the button recieves focus by changing its color
+    /// </summary>
     public void HandleEnter()
     {
-        this.GetComponent<Image>().color = pressedColor;
+        this.GetComponent<Image>().color = selectedColor;
     }
 
+    /// <summary>
+    /// Handles when the button loses focus by resetting its color
+    /// </summary>
     public void HandleExit()
     {
-        this.GetComponent<Image>().color = releasedColor;
+        this.GetComponent<Image>().color = defaultColor;
     }
 
+    /// <summary>
+    /// Handles when the button is left swiped by doing nothing
+    /// </summary>
     public void HandleLeft()
     {
         // Intentionally left blank
     }
 
+    /// <summary>
+    /// Handles when the button is right swiped by pressing it
+    /// </summary>
     public void HandleRight()
     {
         this.GetComponent<Button>().onClick.Invoke();
--- turtleChase/TurtleChase/Assets/Scripts/UI/MenuSlider.cs
+++ turtleChase/TurtleChase/Assets/Scripts/UI/MenuSlider.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class MenuSlider : MonoBehaviour
+/// <summary>
+/// A menu button which can be selected by the face controller
+/// </summary>
+public class MenuSlider : MonoBehaviour, IMenuItem
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Color that the slider takes on to show that it is selected
+    /// </summary>
+    private static readonly Color selectedColor = new Color(0.75f, 1, 1);
+
+    /// <summary>
+    /// Unselected slider color
+    /// </summary>
+    private static readonly Color defaultColor = Color.white;
+
+    /// <summary>
+    /// Handles when the slider recieves focus by changing its color
+    /// </summary>
+    public void HandleEnter()
     {
-        
+        foreach (Image image in this.GetComponentsInChildren<Image>())
+        {
+            image.color = selectedColor;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Handles when the slider loses focus by resetting its color
+    /// </summary>
+    public void HandleExit()
     {
-        
+        foreach (Image image in this.GetComponentsInChildren<Image>())
+        {
+            image.color = defaultColor;
+        }
+    }
+
+    /// <summary>
+    /// Handles when the slider is left swiped by decreasing the value
+    /// </summary>
+    public void HandleLeft()
+    {
+        this.GetComponent<Slider>().value = Mathf.Min(this.GetComponent<Slider>().value - 0.1f, 0);
+    }
+
+    /// <summary>
+    /// Handles when the slider is right swiped by increasing the value
+    /// </summary>
+    public void HandleRight()
+    {
+        this.GetComponent<Slider>().value = Mathf.Min(this.GetComponent<Slider>().value + 0.1f, 1);
     }
 }
