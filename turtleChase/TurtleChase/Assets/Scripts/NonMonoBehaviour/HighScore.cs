c611c61382fc23cf13f3479127b31c1f98414971
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-04-18 01:54:55 -0700

    Created HighScore object to encapsulate a high score

---
+++ turtleChase/TurtleChase/Assets/Scripts/NonMonoBehaviour/HighScore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class HighScore : IComparable<HighScore>
+{
+    /// <summary>
+    /// Score achieved during the round
+    /// </summary>
+    public int Score { get; set; }
+
+    /// <summary>
+    /// X distance traveled during the round
+    /// </summary>
+    public int Distance { get; set; }
+
+    /// <summary>
+    /// Date and time at which the round was completed 
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    public int CompareTo(HighScore other)
+    {
+        return Score.CompareTo(other.Score);
+    }
+}
