19c68e1559f975527e69cb7b301a9588238fa933
Nandeeka Nayak <nandeeka@gmail.com>
2019-04-18 22:25:27 -0700

    Clean up with pylint

--- neural-network/local-processing/label_imgs.py
+++ neural-network/local-processing/label_imgs.py
@@ -1,47 +1,48 @@
-import cv2
+'''
+This module allows a user to label all of the images in all of the directories
+in a given dataset.
+'''
 
 import os
+import cv2
 
-X = 0
-Y = 0
+X_COOR = 0
+Y_COOR = 0
 
-def get_points(event,x,y,flags,param):
+def get_points(event, x, y, flags, param):
     '''
-    Mouse callback function that sets the value of global variables 
+    Mouse callback function that sets the value of global variables
     X and Y to the location of the mouse when it is clicked
     '''
-    global X
-    global Y
+    global X_COOR
+    global Y_COOR
 
     # When the mouse is clicked, set the location
     if event == cv2.EVENT_LBUTTONDOWN:
         print(x, y)
-        X = x
-        Y = y
+        X_COOR = x
+        Y_COOR = y
 
 def get_coors(img_fn):
     '''
     Display an image, and after the user has selected a location in the image,
     write it to a file
     '''
-    global X
-    global Y
-
     # Open the image
     img = cv2.imread(img_fn)
 
     # Set up to read from the mouse
     cv2.namedWindow('image')
-    cv2.setMouseCallback('image',get_points)
+    cv2.setMouseCallback('image', get_points)
 
     # Display the image
-    cv2.imshow('image',img)
+    cv2.imshow('image', img)
     cv2.waitKey(0)
 
     # Write the result to a file
-    f = open(img_fn[:-4] + '.txt', 'w')
-    f.write(str(X) + '\n' + str(Y))
-    f.close()
+    coor_file = open(img_fn[:-4] + '.txt', 'w')
+    coor_file.write(str(X_COOR) + '\n' + str(Y_COOR))
+    coor_file.close()
 
     # Destroy the window
     cv2.destroyAllWindows()
@@ -57,16 +58,16 @@ def draw_labels(img_fn):
     cv2.namedWindow('image')
 
     # Read in the point
-    f = open(img_fn[:-4] + '.txt', 'r')
-    x = int(f.readline())
-    y = int(f.readline())
-    f.close()
+    coors_file = open(img_fn[:-4] + '.txt', 'r')
+    x_coor = int(coors_file.readline())
+    y_coor = int(coors_file.readline())
+    coors_file.close()
 
     # Add the point
-    cv2.circle(img,(x,y), 3, (0,0,255), -1)
+    cv2.circle(img, (x_coor, y_coor), 3, (0, 0, 255), -1)
 
     # Display the image
-    cv2.imshow('image',img)
+    cv2.imshow('image', img)
 
     cv2.waitKey(0)
 
@@ -78,35 +79,35 @@ def process_dir(img_dir, show_comp):
     Process a directory, either by labeling it or displaying the labels
     '''
     img_fns = os.listdir(img_dir)
-    for fn in img_fns:
-        if fn[-4:] == '.jpg':
+    for img_fn in img_fns:
+        if img_fn[-4:] == '.jpg':
             if show_comp:
-                draw_labels(img_dir+fn)
+                draw_labels(img_dir+img_fn)
             else:
-                get_coors(img_dir+fn)
+                get_coors(img_dir+img_fn)
 
 def main():
     '''
-    For every directory in the path, if it has been labeled show labels otherwise allow the user to set them
+    For every directory in the path, if it has been labeled show labels otherwise
+    allow the user to set them
     '''
     # Set up paths
     top_dir = '/Users/nandeekanayak/Downloads/gt_db/'
-    done_dirs = ['s05', 's02', 's34', 's33', 's32', 's35', 's03', 's04', 's50', 
-        's44', 's43', 's26', 's19', 's21', 's17', 's28', 's10', 's42', 's45',
-        's11', 's16', 's29', 's20', 's27', 's18', 's01', 's39', 's06', 's30',
-        's08', 's37', 's09', 's36', 's31', 's38', 's07', 's22', 's25', 's13',
-        's14', 's40', 's47', 's49', 's15', 's12', 's24', 's23', 's48', 's46',
-        's41']
+    done_dirs = ['s05', 's02', 's34', 's33', 's32', 's35', 's03', 's04', 's50', 's44',
+                 's43', 's26', 's19', 's21', 's17', 's28', 's10', 's42', 's45', 's11',
+                 's16', 's29', 's20', 's27', 's18', 's01', 's39', 's06', 's30', 's08',
+                 's37', 's09', 's36', 's31', 's38', 's07', 's22', 's25', 's13', 's14',
+                 's40', 's47', 's49', 's15', 's12', 's24', 's23', 's48', 's46', 's41']
 
     # Check if the user wants to skip the above directories
     show_comp = input('Show completed?') == 'yes'
 
     img_dirs = os.listdir(top_dir)
-    for dir in img_dirs:
-        print(dir)
-        if len(os.path.split(dir)[1]) == 3 and \
-            ((show_comp and dir in done_dirs) or (not show_comp and dir not in done_dirs)):
-            process_dir(top_dir + dir + '/', show_comp)
+    for img_dir in img_dirs:
+        print(img_dir)
+        if len(os.path.split(img_dir)[1]) == 3 and \
+            ((show_comp and img_dir in done_dirs) or (not show_comp and img_dir not in done_dirs)):
+            process_dir(top_dir + img_dir + '/', show_comp)
             cont = input("Continue?")
             if cont != 'yes':
                 break
--- neural-network/local-processing/run_model.py
+++ neural-network/local-processing/run_model.py
@@ -1,14 +1,14 @@
-import cv2
-import os
-import numpy as np
-import time
-import torch
-
-from fastai.vision import *
-
 '''
+Run the fast.ai model
+
 Note: This should be run from the neural-network folder
 '''
+import time
+
+import cv2
+import numpy as np
+
+from fastai.vision import load_learner, Image, pil2tensor, to_np
 
 def create_learner(model_dir, model_fn):
     '''
@@ -24,7 +24,7 @@ def predict_img(learn, np_img):
     # Load the image in the correct format
     img = Image(pil2tensor(np_img, np.float32).div_(255))
 
-    ips, _, _ = learn.predict(img.apply_tfms(None, size=(120,160)))
+    ips, _, _ = learn.predict(img.apply_tfms(None, size=(120, 160)))
     return get_coords(img, ips)
 
 def get_coords(img, ips):
@@ -42,7 +42,7 @@ def display_point(img_fn, point):
 
     # Draw the point
     img = cv2.imread(img_fn)
-    cv2.circle(img, point, 2, (0,0,255), -1)
+    cv2.circle(img, point, 2, (0, 0, 255), -1)
 
     # Display the image
     cv2.imshow('labeled nose', img)
@@ -55,50 +55,54 @@ def process_video(learn):
     '''
     cap = cv2.VideoCapture(0)
     pts = []
-    tm = time.time()
+    curr_time = time.time()
 
     total = 0
     reps = 0
 
-    while(True):
+    while True:
         # Capture frame-by-frame
-        ret, frame = cap.read()
+        _, frame = cap.read()
 
         frame = cv2.resize(frame, (160, 120))
 
         # Process the frame
         point = predict_img(learn, frame)
-        
+
         # Add the point to our list
         pts.append(point)
-        if (len(pts) > 10):
+        if len(pts) > 10:
             pts = pts[1:]
 
         # Draw the last 10 points
         for i in range(len(pts) - 1):
-            cv2.line(frame, pts[i], pts[i+1], (0,0,255), 2)
+            cv2.line(frame, pts[i], pts[i+1], (0, 0, 255), 2)
 
         # Display the resulting frame
-        cv2.imshow('frame',frame)
+        cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print('Average FPS:', reps / total)
             break
 
         reps += 1
-        total += time.time() - tm
-        tm = time.time()
+        total += time.time() - curr_time
+        curr_time = time.time()
 
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+def main():
+    '''
+    Main program
+    '''
+    learner = create_learner('models', 'vanilla_model.pkl')
+    process_video(learner)
 
-    learn = create_learner('models', 'vanilla_model.pkl')
-    process_video(learn)
-    
-    
+if __name__ == '__main__':
+    main()
+
     # Then to display one point, run this
     # img_fn = 'data/nandeeka_test.png'
     # nose_pt = predict_img(learn, cv2.imread(img_fn))
